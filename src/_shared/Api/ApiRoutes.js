55613c7993d746edca8f0f857185e15629f6f223
CristianDelucas <crdelucasnoguero@gmail.com>
2022-09-22 06:13:22 +0200

    new BASE_URL

--- src/_shared/Api/ApiRoutes.js
+++ src/_shared/Api/ApiRoutes.js
@@ -1,5 +1,6 @@
-const BASE_URL= "http://localhost:3000";
 
+//const BASE_URL= "http://localhost:3000";
+const BASE_URL= "https://shrouded-ravine-61384.herokuapp.com";
 const USER=`${BASE_URL}/users`
 const LOGIN=  `${BASE_URL}/users/login`;
 const LOGOUT=  `${BASE_URL}/users/auth/logout`;
